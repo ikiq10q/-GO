a7a620b35a73b09aaa07afcc2ccb914422ed4c89
ikiq10q <thollk1719@gmail.com>
2026-07-31 19:18:45 +0500

    JSON HW

--- Json HW/HomeWork.go
+++ Json HW/HomeWork.go
@@ -1,32 +1,30 @@
 package main
 
-import(
-	import (
+import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
+	"os"
 )
 
-//zadanie 1
-type Movie struct{
-	Title string `json:"title"`
-	Year int `json:"year"`
-	Rating float `json:"rating"`
+// zadanie 1
+type Movie struct {
+	Title  string  `json:"title"`
+	Year   int     `json:"year"`
+	Rating float64 `json:"rating"`
 }
 
 func MovieHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 
-	movie :=Movie{
-		Title: "Karmalogic"
-		Year: "2017"
-		Rating: 8.2
+	movie := Movie{
+		Title:  "Karmalogic",
+		Year:   2017,
+		Rating: 8.2,
 	}
- 	
 
-	jsonData,err := json.Marshal(jsonData, &Movie)
+	jsonData, err := json.Marshal(movie)
 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -37,13 +35,13 @@ func MovieHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", string(jsonData))
 }
 
-//zadanie 2
-type Product struct{
-	Name string `json:"name"`
-	Price int `json:"price"`
+// zadanie 2
+type Product struct {
+	Name  string `json:"name"`
+	Price int    `json:"price"`
 }
 
-func parseProduct (data []byte)(*Product, error){
+func parseProduct(data []byte) (*Product, error) {
 
 	var product Product
 
@@ -52,13 +50,12 @@ func parseProduct (data []byte)(*Product, error){
 		fmt.Println(err)
 		return nil, err
 	}
+	return
 }
 
-
-
-func main(){
+func main() {
 	//zadanie 1
-http.HandleFunc("/tasks", tasksHandler)
+	http.HandleFunc("/movie", MovieHandler)
 
 	fmt.Println("Server is running on port 8080...")
 
@@ -66,20 +63,19 @@ http.HandleFunc("/tasks", tasksHandler)
 	if err != nil {
 		fmt.Println("Ошибка сервера:", err)
 	}
-	
 
 	//zadanie 2
 	jsonData := []byte{
 
-	"name":   "varmilo",
-    "price": 250,
+		"name":  "varmilo",
+		"price": 250,
 	}
 
 	product, err := parseProduct(jsonData)
 
 	if err != nil {
-    fmt.Fprintf(os.Stderr, "Некорректный формат JSON")
-    return
+		fmt.Fprintf(os.Stderr, "Некорректный формат JSON")
+		return
 	}
 
 	fmt.Fprintf(os.Stdout, "Товар: %s, Цена: %d\n", product.Name, product.Price)
